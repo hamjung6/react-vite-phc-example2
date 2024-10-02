e1ec01b587e6094036f88468a010db3653b9a530
WIN-51BV20CU4GG\ICT02_16 <hamjunblack6@gmail.com>
2024-10-02 12:29:29 +0900

    라우터 이해

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,22 @@ import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import BootStrapButton from './BootstrapExample'
 import Header from './components/Header'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import BoardPage from './pages/BoardPage'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
 
 function App() {
-  const hello = 'Hello'
-
-  const style = {
-    fontSize: 30,
-  }
-
   return (
     <>
-      <Header></Header>
-      <div style={style}>{hello}</div>
-      <div>{hello}</div>
-      <BootStrapButton />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/board" element={<BoardPage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
