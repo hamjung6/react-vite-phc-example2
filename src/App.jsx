0ad3ab78558713d13d0ac2d5e582f760a537d6cc
WIN-51BV20CU4GG\ICT02_16 <hamjunblack6@gmail.com>
2024-10-02 11:40:05 +0900

    부트스트랩 적용 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import BootStrapButton from './BootstrapExample'
+import Header from './components/Header'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
 
 function App() {
-
-  const hello = "Hello";
+  const hello = 'Hello'
 
   const style = {
-    fontSize: 30
+    fontSize: 30,
   }
 
   return (
     <>
+      <Header></Header>
       <div style={style}>{hello}</div>
       <div>{hello}</div>
+      <BootStrapButton />
     </>
   )
 }
