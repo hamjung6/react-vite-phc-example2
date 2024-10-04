21d192df150ca61b246c3a2293ee23b76ba5c7cb
WIN-51BV20CU4GG\ICT02_16 <hamjunblack6@gmail.com>
2024-10-04 11:17:58 +0900

    조건부 랜더링 완료

--- src/route/Routes.jsx
+++ src/route/Routes.jsx
@@ -3,6 +3,7 @@ import HomePage from '../pages/HomePage'
 import BoardPage from '../pages/BoardPage'
 import Profile from '../pages/Profile'
 import PropsPage from '../pages/PropsPage'
+import ThreePage from '../pages/ThreePage'
 
 // React Routes, Route 사용 시 배열로 관리하기
 const routes = [
@@ -25,6 +26,11 @@ const routes = [
     path: '/props',
     element: <PropsPage />,
     title: '프롭스',
+  },
+  {
+    path: '/three',
+    element: <ThreePage />,
+    title: '조건부랜더링',
   },
 ]
 
