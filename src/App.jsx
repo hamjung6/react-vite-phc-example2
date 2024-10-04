7ba380a28a7ec9258e20ab23f6e6cf15d5852ad7
WIN-51BV20CU4GG\ICT02_16 <hamjunblack6@gmail.com>
2024-10-04 09:37:01 +0900

    라우터 이해2

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import BoardPage from './pages/BoardPage'
 import Profile from './pages/Profile'
+import routes from './route/Routes'
+import HeaderNav from './components/HeaderNav'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
@@ -14,10 +16,17 @@ function App() {
   return (
     <>
       <BrowserRouter>
+        <HeaderNav></HeaderNav>
         <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/board" element={<BoardPage />}></Route>
-          <Route path="/profile" element={<Profile />}></Route>
+          {routes.map((route, index) => {
+            return (
+              <Route
+                key={route.path || index}
+                path={route.path}
+                element={route.element}
+              ></Route>
+            )
+          })}
         </Routes>
       </BrowserRouter>
     </>
--- src/route/Routes.jsx
+++ src/route/Routes.jsx
@@ -1,8 +1,25 @@
 import React, { Component } from 'react'
+import HomePage from '../pages/HomePage'
+import BoardPage from '../pages/BoardPage'
+import Profile from '../pages/Profile'
 
-export default [
+// React Routes, Route 사용 시 배열로 관리하기
+const routes = [
   {
     path: '/',
-    component: Home,
+    element: <HomePage />,
+    title: 'Home',
+  },
+  {
+    path: '/board',
+    element: <BoardPage />,
+    title: 'Board',
+  },
+  {
+    path: '/profile',
+    element: <Profile />,
+    title: 'Profile',
   },
 ]
+
+export default routes
