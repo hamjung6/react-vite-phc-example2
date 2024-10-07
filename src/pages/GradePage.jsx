c216ea410b4c9f21a79362aa63c390a3c2dcedec
WIN-51BV20CU4GG\ICT02_16 <hamjunblack6@gmail.com>
2024-10-07 12:19:51 +0900

    성적표2 개선

--- src/pages/GradePage.jsx
+++ src/pages/GradePage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import InputGrade from '../components/InputGrade'
+import InputGrade from './../components/InputGrade'
+import InputGrade2 from '../components/InputGrade2'
 
 const GradePage = () => {
   return (
     <div className="text-center mt-5">
       <InputGrade />
+      <hr></hr>
+      <InputGrade2 />
     </div>
   )
 }
